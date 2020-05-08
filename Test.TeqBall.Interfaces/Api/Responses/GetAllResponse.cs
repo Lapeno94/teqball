7d79031122e9cad5ee45561a1b39f73c214e9ff0
Tomi <lapeno94@gmail.com>
2020-05-08 15:37:55 +0200

    add interfaces proj

---
+++ Test.TeqBall.Interfaces/Api/Responses/GetAllResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.TeqBall.Interfaces.Api.Responses
+{
+    class GetAllResponse
+    {
+    }
+}
---
+++ Test.TeqBall.Interfaces/Endpoints.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.TeqBall.Interfaces
+{
+    public struct Endpoints
+    {
+        public struct Appointment
+        {
+            public static string GetAll = "/asd";
+        }
+    }
+}
